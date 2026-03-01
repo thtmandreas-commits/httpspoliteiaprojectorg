0416ae6c4c1223bc4977827f672e7a7068ee0bcf
Tom Hestås <tomhestas@Tom-sin-MacBook-Air.local>
2026-03-01 03:20:37 +0100

    Align app display name to The Great Doom Loop

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.politeia.doomloop',
+  appName: 'The Great Doom Loop',
+  webDir: 'dist',
+};
+
+export default config;
